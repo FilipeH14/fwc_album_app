9c790b5ad82880663d0aa5150743bffc35908719
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-06-24 11:22:05 -0300

    implements login repository and service

---
+++ lib/app/services/login/login_service_impl.dart
@@ -0,0 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fwc_album_app/app/repository/auth/auth_repository.dart';
+import 'package:fwc_album_app/app/services/login/login_service.dart';
+
+class LoginServiceImpl implements LoginService {
+  final AuthRepository _authRepository;
+
+  LoginServiceImpl({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  @override
+  Future<void> execute(
+      {required String email, required String password}) async {
+    final accessToken =
+        await _authRepository.login(email: email, password: password);
+
+    final sp = await SharedPreferences.getInstance();
+
+    sp.setString('accessToken', accessToken);
+  }
+}
