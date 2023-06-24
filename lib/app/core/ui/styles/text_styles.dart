db86f157d0522eaf334a256419a7c6d1ba4c79e7
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-06-23 23:20:28 -0300

    build register screen

--- lib/app/core/ui/styles/text_styles.dart
+++ lib/app/core/ui/styles/text_styles.dart
@@ -73,6 +73,9 @@ class TextStyles {
 
   TextStyle get titleWhite =>
       textPrimaryFontBold.copyWith(color: Colors.white, fontSize: 22);
+
+  get titleBlack =>
+      textPrimaryFontBold.copyWith(color: Colors.black, fontSize: 22);
 }
 
 extension TextStylesExtensions on BuildContext {
